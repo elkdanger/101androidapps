280e83b1d24df13180d0cae14916c270c0ebe20d
Steve Hobbs <elkdanger@googlemail.com>
2014-08-03 22:24:30 +0100

    Add state saving to Tally

--- Tally/app/src/main/java/com/stevescodingblog/androidapps/tally/MainActivity.java
+++ Tally/app/src/main/java/com/stevescodingblog/androidapps/tally/MainActivity.java
@@ -1,14 +1,19 @@
 package com.stevescodingblog.androidapps.tally;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.prefs.Preferences;
 
 public class MainActivity extends Activity {
 
-    int _tally = 1;
+    int _tally = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +25,8 @@ public class MainActivity extends Activity {
         layoutRoot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                _tally += 1;
-                updateUi();
+            _tally += 1;
+            updateUi();
             }
         });
 
@@ -29,16 +34,48 @@ public class MainActivity extends Activity {
         resetButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                _tally = 1;
-                updateUi();
+            _tally = 0;
+            updateUi();
             }
         });
+
+        this.updateUi();
     }
 
     protected void updateUi() {
 
         final TextView tallyDisplay = (TextView)this.findViewById(R.id.tallyDisplay);
         tallyDisplay.setText(Integer.toString(this._tally));
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.loadState();
+        this.updateUi();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveState();
+    }
+
+    private void loadState() {
+        // See if we have previously saved the count, and load it
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        this._tally = prefs.getInt("count", 0);
+    }
+
+    private void saveState() {
+        // Get the preferences and the editor for the preferences
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // Store our count..
+        editor.putInt("count", this._tally);
+
+        // Save changes
+        editor.commit();
     }
 }
